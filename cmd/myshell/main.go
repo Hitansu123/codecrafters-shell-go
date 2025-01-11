113cb9586b83cb256fdb440b8d2289031c437654
Hitansu123 <hitansupanda123@gmail.com>
2025-01-11 23:22:04 +0530

    inplementing exit command

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -13,15 +13,21 @@ var _ = fmt.Fprint
 func main() {
 	// Uncomment this block to pass the first stage
 	fmt.Fprint(os.Stdout, "$ ")
-
 	// Wait for user input
-	userInput, err := bufio.NewReader(os.Stdin).ReadString('\n')
-	if err != nil {
-		fmt.Println("invalid_command: command not found")
+	userInput := bufio.NewReader(os.Stdin)
+
+	for {
+		command, err := userInput.ReadString('\n')
+
+		if err != nil {
+			fmt.Println("invalid_command: command not found")
+			os.Exit(1)
+		}
+		command = strings.TrimSpace(command)
+		if command == "exit 0" {
+			os.Exit(0)
+		}
+		fmt.Fprintf(os.Stdout, "%s: command not found\n", command)
+		fmt.Fprint(os.Stdout, "$ ")
 	}
-	userInput = strings.TrimSpace(userInput)
-	output := fmt.Sprintf("%s: command not found", userInput)
-	fmt.Fprint(os.Stdout, output)
-	fmt.Println()
-	main()
 }

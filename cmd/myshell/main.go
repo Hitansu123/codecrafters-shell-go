90964154ccb1b4315341e2d8e98383082b46fd72
Hitansu123 <hitansupanda123@gmail.com>
2025-01-12 15:50:41 +0530

    inplementing type command

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -10,6 +10,20 @@ import (
 // Ensures gofmt doesn't remove the "fmt" import in stage 1 (feel free to remove this!)
 var _ = fmt.Fprint
 
+func echocmd(first []string) string {
+	var after string
+	for _, item := range first[1:] {
+		after = after + item
+		after += " "
+	}
+	return after
+}
+func typecmd(first []string) string {
+	if first[1] == "echo" || first[1] == "exit" {
+		return fmt.Sprintf("%s is a build in command", first[1])
+	}
+	return fmt.Sprintf("invalid_command: not found")
+}
 func main() {
 	// Uncomment this block to pass the first stage
 	fmt.Fprint(os.Stdout, "$ ")
@@ -26,12 +40,9 @@ func main() {
 		command = strings.TrimSpace(command)
 		first := strings.Split(command, " ")
 		if first[0] == "echo" {
-			var after string
-			for _, item := range first[1:] {
-				after = after + item
-				after += " "
-			}
-			fmt.Println(after)
+			fmt.Println(echocmd(first))
+		} else if first[0] == "type" {
+			fmt.Println(typecmd(first))
 		} else if command == "exit 0" {
 			os.Exit(0)
 		} else {
